fc8337f5f581d538d695c6cabafa76bcd47aaa42
dariomartinovski <master15820@gmail.com>
2025-03-28 19:54:50 +0100

    Added Request Camera Permission

---
+++ app/src/main/java/mk/ukim/finki/syncit/utils/RequestCameraPermission.kt
@@ -0,0 +1,28 @@
+package mk.ukim.finki.syncit.utils
+
+import android.Manifest
+import android.widget.Toast
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.platform.LocalContext
+
+@Composable
+fun RequestCameraPermission(onPermissionGranted: () -> Unit) {
+    val context = LocalContext.current
+    val launcher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestPermission(),
+        onResult = { isGranted ->
+            if (isGranted) {
+                onPermissionGranted()
+            } else {
+                Toast.makeText(context, "Camera permission denied", Toast.LENGTH_SHORT).show()
+            }
+        }
+    )
+
+    LaunchedEffect(Unit) {
+        launcher.launch(Manifest.permission.CAMERA)
+    }
+}
